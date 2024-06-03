8c2fe5ac85fe1ac96e73f851722d52ed4e8e2e80
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-06-03 18:53:04 +0530

    solved

---
+++ time_needed_to_buy_tickets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int count = 0;
+        while(true){
+            if(tickets[k] == 0)
+                break;
+            for(int i=0; i<tickets.size(); i++){
+                if(tickets[k] == 0)
+                    break;
+                if(tickets[i] > 0){
+                    tickets[i] -= 1;
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
+};
